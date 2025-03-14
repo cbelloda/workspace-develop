eb2e8d236a9e92fb6e0df66c4bf19301e1dd0db9
cbelloda <cbelloda@gmail.com>
2025-03-14 01:10:55 -0500

    Create postCreateCommand.sh

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Configurando el entorno de Spring WebFlux..."
+
+# Verificar la versión de Java
+java -version
+
+echo "Configuración completada. ¡Listo para desarrollar con Spring WebFlux!"
